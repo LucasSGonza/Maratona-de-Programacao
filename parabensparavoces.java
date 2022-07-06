9fa5d096eb1296a52e9e2fe3762f641735b43f33
Lucas <107816056+LucasSGonza@users.noreply.github.com>
2022-07-06 11:47:59 -0300

    Delete parabensparavoces.java

--- parabensparavoces.java
+++
@@ -1,9 +0,0 @@
-
-public class parabensparavoces {
-    public static void main(String[]args){       
-        int submissoes = 1652;
-        int times = 118;
-        int media = submissoes/times;
-        System.out.print(media);
-    }
-}
